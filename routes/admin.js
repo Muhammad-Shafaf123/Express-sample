d1acaea3155bc1b6871d874c0729a80b1e93f6d8
Muhammad Shafaf <shafaf@zennode.com>
2022-12-30 09:22:07 +0530

    route setup, not completed

---
+++ routes/admin.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  let products = [
+    {
+      name : 'IPHONE',
+      category : 'Mobile',
+      description : 'This is a good phone!',
+      image : 'https://cdn.dxomark.com/wp-content/uploads/medias/post-125834/Apple-iPhone-14_FINAL_featured-image-packshot-review.jpg'
+    },
+    {
+      name : 'Samsung',
+      category : 'Mobile',
+      description : 'This is a good phone!',
+      image : 'https://images.samsung.com/is/image/samsung/p6pim/in/2202/gallery/in-galaxy-s22-ultra-s908-413016-sm-s908ezwginu-530969260?$650_519_PNG$'
+    },
+    {
+      name : 'POCO',
+      category : 'Mobile',
+      description : 'This is a good phone!',
+      image : 'https://www.notebookcheck.net/uploads/tx_nbc2/4_zu_3_Teaser_neu.jpeg'
+    },
+    {
+      name : 'Huawie',
+      category : 'Mobile',
+      description : 'This is a good phone!',
+      image : 'https://consumer.huawei.com/content/dam/huawei-cbg-site/common/mkt/homepage/new-homepage/model-banner/nova10-pro.png'
+    },
+  ]
+  res.render('admin/view-products',{role : true, products : products});
+});
+
+router.get('admin/add-product', function(req, res, next) {
+  res.render('admin/view-products',{role : true, products : products});
+})
+
+module.exports = router;
